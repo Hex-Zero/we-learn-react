54d2e15b8bd203793add168671ae29c67d8db1e9
Maxwell <hex0@live.com>
2022-05-17 22:08:18 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Goodbye, Greeting } from "./components/Greeting";
+
+function App() {
+  return (
+    <div className="App">
+      <Greeting />
+      <Goodbye />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Greeting.js
@@ -0,0 +1,17 @@
+export function Greeting() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>This is a simple react component</p>
+    </div>
+  );
+}
+
+export function Goodbye() {
+  return (
+    <div>
+      <h1>Goodbye World</h1>
+      <p>This is a simple react component</p>
+    </div>
+  );
+}
